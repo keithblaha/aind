b56f2ab305a8db1498f64c49efe680d90425b256
Keith Blaha <keith.blaha@gmail.com>
2017-08-19 16:56:58 -0700

    Setup keras/tf docker container and add aind2-dl

---
+++ aind2-dl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "$1" ]; then
+  echo "Requires valid target directory"
+  exit 1
+else
+  DIR="$1"
+fi
+
+docker build -t keithblaha/aind2-dl -f Dockerfile-aind2-dl .
+
+nvidia-docker run                         \
+  --net=host                              \
+  -e=DISPLAY                              \
+  -v=$HOME/.Xauthority:/root/.Xauthority  \
+  -v=$PWD/$DIR:/src                       \
+  -p=8888                                 \
+  -it keithblaha/aind2-dl
