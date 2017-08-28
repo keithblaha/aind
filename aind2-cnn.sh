2b75a7ff6b6d39f2c0e80758ba6442fda1724615
Keith Blaha <keith.blaha@gmail.com>
2017-08-27 18:25:26 -0700

    Updates for dog cnn project

--- aind2-cnn.sh
+++ aind2-cnn.sh
@@ -9,10 +9,13 @@ fi
 
 docker build -t keithblaha/aind2-dog -f Dockerfile-aind2-cnn .
 
-nvidia-docker run                         \
-  --net=host                              \
-  -e=DISPLAY                              \
-  -v=$HOME/.Xauthority:/root/.Xauthority  \
-  -v=$PWD/$DIR:/src                       \
-  -p=8888                                 \
+nvidia-docker run                                       \
+  --net=host                                            \
+  -e=DISPLAY                                            \
+  -v=$HOME/.Xauthority:/root/.Xauthority                \
+  -v=$PWD/$DIR:/src                                     \
+  -v=$PWD/dogImages:/src/dogImages                      \
+  -v=$PWD/lfw:/src/lfw                                  \
+  -v=$PWD/bottleneck_features:/src/bottleneck_features  \
+  -p=8888                                               \
   -it keithblaha/aind2-dog
